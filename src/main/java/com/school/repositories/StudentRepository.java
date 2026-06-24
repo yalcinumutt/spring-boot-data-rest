b5e96c00fed0293eb22dfdd83fbeaaf3c6e49ce3
yalcinumut <yalcin.umutali@gmail.com>
2026-06-24 11:25:13 +0300

    chore: package update

--- src/main/java/com/school/repositories/StudentRepository.java
+++ src/main/java/com/school/repositories/StudentRepository.java
@@ -1,13 +1,13 @@
 package com.school.repositories;
 
 import com.school.entities.Student;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "students", path = "students")
-public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
+public interface StudentRepository extends JpaRepository<Student, Long> {
     List<Student> findByName(@Param("name") String name);
 }
